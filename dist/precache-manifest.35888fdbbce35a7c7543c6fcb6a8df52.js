d4a86606e4d6e5df7a5a139d59d69a2299809294
Jozef Lacko <jlacko27@gmail.com>
2018-04-25 22:57:14 +0200

    t

---
+++ dist/precache-manifest.35888fdbbce35a7c7543c6fcb6a8df52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c996f97b229a2f22ce",
+    "url": "/js/vendor.10c996f9.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "b1c2329410fb2aec205f",
+    "url": "/js/app.b1c23294.js"
+  },
+  {
+    "revision": "e47cadebcf35db66fc48de881553aaea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c2329410fb2aec205f",
+    "url": "/css/app.480d0da6.css"
+  }
+];
